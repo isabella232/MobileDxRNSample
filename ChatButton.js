d11d42e7ec311bb7fa162f3063a97dee266e4f76
Eliza Koren <eliza.koren@logmein.com>
2021-11-11 18:32:41 +0200

    make iOS sample work with module

--- ChatButton.js
+++ ChatButton.js
@@ -2,13 +2,13 @@ import React, { useState, useEffect } from 'react';
 import { NativeModules, Button } from 'react-native';
 
 import GenDXChatModule from 'genesys-mobiledx-chat-rn-module';
-
+import { presentMessageController } from 'genesys-mobiledx-chat-rn-module';
 
 const ChatButton = () => {
   const onPress = () => {
-
-    GenDXChatModule.startChatComponent("f8aad9d7-f8e7-48e9-ab02-eef92bc4fd2f", "inindca.com",
-    "com.genesys.messenger.poc", true, "tehila.rozin@genesys.com", "666-666-66", "Fame", "RO");
+    presentMessageController();
+    // GenDXChatModule.startChatComponent("f8aad9d7-f8e7-48e9-ab02-eef92bc4fd2f", "inindca.com",
+    // "com.genesys.messenger.poc", true, "tehila.rozin@genesys.com", "666-666-66", "Fame", "RO");
   }
 
   return (
